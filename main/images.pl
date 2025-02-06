03ca427ffa8e86d9764dfc26a16f6f491b67ef81
Doraemon-fan12 <monovyt@gmail.com>
2025-02-06 14:39:49 +0000

    update

---
+++ main/images.pl
@@ -0,0 +1,88 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate images original text with physical files.
+
+
+$key = q/{frame}%latex2htmlidmarker164[allowframebreaks]{GradientofaScalar-ValuedFunctionegradientvectorperpendiculartotheplane.}{figure{{par{{frame};MSF=1.6;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img8.svg"
+ ALT="\begin{frame}
+% latex2html id marker 164
+[allowframebreaks]{Gradient of a Scalar...
+...nd the gradient vector perpendicular to the plane.}
+\end{figure}\par
+\end{frame}">|; 
+
+$key = q/{frame}%latex2htmlidmarker42[allowframebreaks]frametitle{GeometricMeaningofthedewithitstangentvectorandthetangentlineat{t=pislash4{.}{frame};MSF=1.6;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img3.svg"
+ ALT="\begin{frame}
+% latex2html id marker 42
+[allowframebreaks]
+\frametitle{Geometric...
+...t circle with its tangent vector and the tangent line at $t=\pi/4$.}
+\end{frame}">|; 
+
+$key = q/{frame}frametitle{Introduction}itemize{itemtextbf{Overview:}itemize{itemExtendstnsandmodelingreal-worldphenomena.itemize{itemize{{par{{frame};MSF=1.6;TAGS=R;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img2.svg"
+ ALT="\begin{frame}
+\frametitle{Introduction}
+\begin{itemize}
+\item \textbf{Overview:...
+... and modeling real-world phenomena.
+\end{itemize} \end{itemize}\par
+\end{frame}">|; 
+
+$key = q/{frame}titlepage<comment_mark>59{frame};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img1.svg"
+ ALT="\begin{frame}
+\titlepage % Print the title page as the first slide
+\end{frame}">|; 
+
+$key = q/{frame}{Example:ClimbingaHill}Imagineyouarehikingonahillwheretheheightatanypointepestdirection).itemize{{par{{centering{<comment_mark>80{par{{frame};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img5.svg"
+ ALT="\begin{frame}{Example: Climbing a Hill}
+Imagine you are hiking on a hill where ...
+...h=0.5\linewidth]{gradienthill.png} % Add a relevant figure here
+\par
+\end{frame}">|; 
+
+$key = q/{frame}{GeometricMeaning}itemize{itemThetextbf{gradientvector}alwayspointsinthedradientistextbf{perpendicular}tothelevelcurves(contourlines).itemize{{frame};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img6.svg"
+ ALT="\begin{frame}{Geometric Meaning}
+\begin{itemize}
+\item The \textbf{gradient ve...
+...bf{perpendicular} to the level curves (contour lines).
+\end{itemize}\end{frame}">|; 
+
+$key = q/{frame}{Huge{{{centerline{TheEnd}{}{frame};AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img9.svg"
+ ALT="\begin{frame}
+\Huge {\centerline{The End}}
+\end{frame}">|; 
+
+$key = q/{frame}{PhysicalMeaning(Real-WorldExamples)}Thegradientisusefulinmanyareasofscieientofelevation.itemize{{par{{centering{<comment_mark>81{par{{frame};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img7.svg"
+ ALT="\begin{frame}{Physical Meaning (Real-World Examples)}
+The gradient is useful in...
+...inewidth]{gradient_real_world.png} % Add a relevant figure here
+\par
+\end{frame}">|; 
+
+$key = q/{frame}{UnderstandingtheGradientofaFunction}{par{textbf{WhatistheGradient?}Thetehowmuch{(f){changesaswemoveinthe{(y){-direction.itemize{{par{{frame};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img4.svg"
+ ALT="\begin{frame}{Understanding the Gradient of a Function}
+\par
+\textbf{What is the...
+...f \) changes as we move in the \( y \)-direction.
+\end{itemize}\par
+\end{frame}">|; 
+
+1;
+
